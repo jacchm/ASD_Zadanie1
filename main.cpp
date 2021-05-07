74bab2e54c38be20bbf6f0d79dff6896917313a7
Jacek Chmiel <jacekchmiel03@gmail.com>
2021-05-07 21:54:24 +0200

    Small fixes

--- main.cpp
+++ main.cpp
@@ -7,33 +7,31 @@ struct QueueNode {
     QueueNode *next;
 };
 
-struct QueueImpl {
-    QueueNode *front;
-    QueueNode *rear;
-    int size;
+struct Queue {
+    QueueNode *front = nullptr;
+    QueueNode *rear = nullptr;
+    int size = 0;
 };
 
-void add(QueueImpl *queue, int x);
+void add(Queue *queue, int x);
 
-bool next(QueueImpl *queue);
+bool next(Queue *queue);
 
-int first(QueueImpl *queue);
+int first(Queue *queue);
 
-bool isEmpty(QueueImpl *queue);
+bool isEmpty(Queue *queue);
 
-void displayQueue(QueueImpl *queue);
+void displayQueue(Queue *queue);
 
 /*
  * main() created for testing
  */
 int main() {
 
-    auto *queue = new QueueImpl;
-    queue->front = nullptr;
-    queue->rear = nullptr;
+    auto *queue = new Queue;
 
+    cout << "Size of a queue: " << queue->size << endl;
     cout << "queue.front = " << queue->front << " queue.rear = " << queue->rear << endl;
-
 
     add(queue, 1);
     add(queue, 2);
@@ -57,9 +55,6 @@ int main() {
 
     cout << "First: " << first(queue) << endl;
 
-
-
-
     cout << "End of tests." << endl;
     return 0;
 }
@@ -67,16 +62,16 @@ int main() {
 /*
  * Typical name for this function is enqueue();
  */
-void add(QueueImpl *queue, int x) {
+void add(Queue *queue, int x) {
     QueueNode *newNode = new QueueNode;
     newNode->next = nullptr;
     newNode->value = x;
 //    cout << "newnode.next = " << newNode->next << " newnode.value = " << newNode->value << endl;
 
-    if (queue->front == nullptr && queue->rear == nullptr) {
+    if (queue->front == nullptr && queue->rear == nullptr) {    // kolejka jest pusta
         queue->front = newNode;
         queue->rear = newNode;
-    } else {
+    } else {                                                    // kolejka nie jest pusta
         queue->rear->next = newNode;
         queue->rear = newNode;
     }
@@ -86,34 +81,36 @@ void add(QueueImpl *queue, int x) {
 /*
  * Typical name for this function is dequeue();
  */
-bool next(QueueImpl *queue) {
+bool next(Queue *queue) {
     if (isEmpty(queue)) {
         cout << "Queue is empty." << endl;
         return false;
     }
     if (queue->front == queue->rear) {
-        free(queue->front);
+        QueueNode *ptr = queue->front;
         queue->front = nullptr;
         queue->rear = nullptr;
+        delete ptr;
     } else {
-        QueueNode *front = queue->front;
+        QueueNode *ptr = queue->front;
         queue->front = queue->front->next;
-        delete front;
+        delete ptr;
     }
     queue->size--;
     return true;
 }
 
-int first(QueueImpl *queue) {
+int first(Queue *queue) {
     if (isEmpty(queue)) {
         cout << "Queue is empty." << endl;
+        // co wpisać jako wartość
         return -1;
     }
     return queue->front->value;
 }
 
-bool isEmpty(QueueImpl *queue) {
-    if (queue->front == nullptr && queue->rear == nullptr) {
+bool isEmpty(Queue *queue) {
+    if (queue->front == nullptr) {
         return true;
     }
     return false;
@@ -123,7 +120,7 @@ bool isEmpty(QueueImpl *queue) {
  * Function created for testing.
  * Prints the whole Queue.
  */
-void displayQueue(QueueImpl *queue) {
+void displayQueue(Queue *queue) {
     if (isEmpty(queue)) {
         cout << "Queue is empty." << endl;
     }
